86d0d21f34b143de6f9fe57178bdb0aa9ec2239c
Malo Lebrin <malolebrin@gmail.com>
2020-11-06 11:04:15 +0100

    end of exercice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import "./App.scss";
+import Header from "./header/Header";
+import Switch from "./switch/Switch";
 
-function App() {
-  return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
-    </div>
-  );
-}
+const App = () => {
+    const [firstSwitch, setFirstSwitch] = useState(false);
+    const [secondSwitch, setSecondSwitch] = useState(false);
+    const [thirdSwitch, setThirdSwitch] = useState(false);
+    return (
+        <div className="App">
+            <Header />
+            <div className="container">
+                <div className="switches">
+                    <Switch switchButton={firstSwitch} setSwitch={setFirstSwitch} />
+                    <Switch switchButton={secondSwitch} setSwitch={setSecondSwitch} />
+                    <Switch switchButton={thirdSwitch} setSwitch={setThirdSwitch} />
+                </div>
+                <div className={`go-no-way ${firstSwitch && secondSwitch && thirdSwitch ? "go" : "no-way"}`}>
+                    {firstSwitch && secondSwitch && thirdSwitch ? "Let's Go" : "No-Way Dude"}
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "../title/Title";
+
+const Header = (props) => {
+    return (
+        <header className="header">
+            <Title title="Ready To Go" />
+        </header>
+    );
+};
+
+export default Header;
